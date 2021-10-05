9e7e23c2bd6067f6649925d83dc4264113edc647
anakomase <elizakomase@gmail.com>
2021-10-04 21:34:35 -0300

    primeiros passos com c#

---
+++ CriandoVariaveis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto crianco variaveis");
+
+
+            int idade;
+            idade = 32;
+
+            Console.WriteLine(idade);
+                 
+            Console.WriteLine("execução finalizada. tecle enter para sair");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ meuPrimeiroProgramaCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace meuPrimeiroProgramaCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("a execução acabou tecle enter para finalizar");
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
